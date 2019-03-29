f1fb16fdb0e753968b3c92ae7c85ae15e804100a
almex <alexeybezrukov2@gmail.com>
2019-03-29 20:23:49 +0300

    home

--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
+import Home from './home/containers/Home'
 import Products from './products/containers/Products'
 import ProductInfo from './productInfo/containers/ProductInfo'
 
 const coreURL = process.env.CORE_URL ? process.env.CORE_URL : '/'
 
 const Main = () => (
+  
   <main className="main">
     <div className="wrapper">
       <Switch>
-        <Route exact path={coreURL} component={Products} />
-        <Route path="/:productType/:productID" component={ProductInfo} />
+        <Route exact path={coreURL} component={Home} />
+        <Route exact path={coreURL + ":productType"} component={Products} />
+        <Route exact path="/:productType/:productID" component={ProductInfo} />
       </Switch>
     </div>
   </main>
